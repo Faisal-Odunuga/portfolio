689da4c2c5c829ada738a290057d70d68864a83d
Faisal Odunuga <faisalodunuga@gmail.com>
2025-07-25 10:38:53 +0100

    fixed img not showing

--- src/shared/projects/AllProjects.jsx
+++ src/shared/projects/AllProjects.jsx
@@ -6,7 +6,7 @@ const AllProjects = () => {
     {
       title: "Cookit 👨🏿‍🍳",
       desc: "A modern recipe discovery platform developed using React and React Router, with Tailwind CSS for styling. Implements real-time search filtering, dynamic routing, and mobile-first responsiveness for seamless UX across devices.",
-      image: "../../../public/images/cookit.png",
+      image: "/images/cookit.png",
       stack: ["React", "Tailwind CSS", "React Router", "Toastify", "Vercel"],
       link: "https://cookit-now.vercel.app/",
       github: "https://github.com/Faisal-Odunuga/Cookit",
@@ -14,7 +14,7 @@ const AllProjects = () => {
     {
       title: "The React Quiz 🧠",
       desc: "A performant quiz application built with React, Vite, and Tailwind CSS. It features dynamic question rendering, real-time score computation, and a fully responsive UI optimized for speed and interactivity.",
-      image: "../../../public/images/react-quiz.png",
+      image: "/images/react-quiz.png",
       stack: ["React", "Tailwind CSS", "React Router", "Vercel"],
       link: "https://the-react-quiz-omega.vercel.app/",
       github: "https://github.com/Faisal-Odunuga/the-react-quiz",
@@ -22,7 +22,7 @@ const AllProjects = () => {
     {
       title: "MTN (Clone)",
       desc: "A fully responsive, high-fidelity clone of the MTN Nigeria homepage built with Tailwind CSS. This project demonstrates advanced use of utility-first styling, responsive design principles, layout structuring, and modern frontend development techniques.",
-      image: "../../../public/images/mtn.png",
+      image: "/images/mtn.png",
       stack: ["React", "Tailwind CSS", "Vercel"],
       link: "https://mtn-clone.vercel.app/",
       github: "https://github.com/Faisal-Odunuga/mtn-clone",
